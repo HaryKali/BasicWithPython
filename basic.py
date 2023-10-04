c255cd6496e0edc343a72fe962e3f69ab17ea167
harykali <harykali@outlook.com>
2023-10-04 15:21:28 +0800

    parser v0.001

--- basic.py
+++ basic.py
@@ -17,6 +17,8 @@ TT_MUL = "MUL"
 """
 POSTION
 """
+
+
 class Postion:
     def __init__(self, idx, ln, col, fn, ftxt):
         self.idx = idx
@@ -143,11 +145,87 @@ class Lexer:
 
 
 """
+NODES
+"""
+
+
+class NumberNode:
+    def __init__(self,tok):
+        self.tok = tok
+
+    def __repr__(self):
+        return f"{self.tok}"
+
+
+class BinOpNode:
+    def __init__(self, left_node, op_tok, right_node):
+        self.left_node = left_node
+        self.op_tok = op_tok
+        self.right_node = right_node
+
+    def __repr__(self):
+        return f"({self.left_node}, {self.op_tok}, {self.right_node})"
+
+
+"""
+PARSER
+"""
+
+
+class Parser:
+
+    def __init__(self, tokens):
+        self.tokens = tokens
+        self.tok_idx = -1
+        self.advance()
+        # maybe
+        # self.current_tok = ""
+
+    def parser(self):
+        res = self.expr()
+        return res
+
+    def advance(self):
+        self.tok_idx += 1
+        if self.tok_idx < len(self.tokens):
+            self.current_tok = self.tokens[self.tok_idx]
+        return self.current_tok
+
+    def factor(self):
+        tok = self.current_tok
+        if tok.type in (TT_INT, TT_FLOAT):
+            self.advance()
+            return NumberNode(tok)
+
+    def term(self):
+        return self.bin_op(self.factor,(TT_MUL,TT_DIV))
+    def expr(self):
+        return self.bin_op(self.term, (TT_PLUS, TT_MINUS))
+
+    def bin_op(self, func, ops):
+        left = func()
+
+        while self.current_tok.type in ops:
+            op_tok = self.current_tok
+            self.advance()
+            right = func()
+            left = BinOpNode(left, op_tok, right)
+        return left
+
+
+"""
 test
 """
 
 
 def run(fn, text):
+    # Generate tokens
     lexer = Lexer(fn, text)
-    token, error = lexer.make_tokens()
-    return token, error
+    tokens, error = lexer.make_tokens()
+    if error:
+        return None,error
+    # return tokens, error
+    # Generate AST
+    parser = Parser(tokens=tokens)
+    ast = parser.parser()
+    return ast, None
